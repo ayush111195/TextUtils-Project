7d7d1e5e3feb722e5e810ad6555fa03751fbcc97
Arpit gupta <gupta.arpit@indiamart.com>
2023-04-16 21:25:10 +0530

    props

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar(props) {                   // react fution based 
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
     <div className="container-fluid">
-      <a className="navbar-brand" href="/">{props.title}</a>
+      <a className="navbar-brand" href="/">{props.title}</a> {/*TITIL KI VALUE PROPES SE DENE KE LEYE */}
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -15,7 +15,7 @@ export default function Navbar(props) {                   // react fution based 
             <a className="nav-link active" aria-current="page" href="/">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/">{props.aboutText}</a>
+            <a className="nav-link" href="/">{props.aboutText}</a> {/*TITIL KI VALUE PROPES SE DENE KE LEYE */}
           </li>
         </ul>
         <form className="d-flex" role="search">
@@ -28,11 +28,11 @@ export default function Navbar(props) {                   // react fution based 
   
   )
 }
-navigator.propTypes={title: PropTypes.string.isRequired,
+Navbar.propTypes={title: PropTypes.string.isRequired,         //  {/*props ki value ka type set karne ke leye  */}
                     aboutText:PropTypes.string.isRequired}
 
 
-Navbar.defaultProps={
+Navbar.defaultProps={                                         // {/* props ki default  value set karne ke leye  */}
     title:'set title here',
     aboutText:'About text here'
 }                    
